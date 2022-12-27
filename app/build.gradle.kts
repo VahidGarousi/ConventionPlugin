d6e9f9387d546edc063377396641112978cb7780
Vahid Garousi <garousiv@gmail.com>
2022-12-27 14:22:23 +0330

    Enable Convention Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application")
+    id("gradleconvention.android.application")
     kotlin("android")
 }
 
--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     `kotlin-dsl`
 }
 
-group = "com.google.samples.apps.nowinandroid.buildlogic"
+group = "dev.garousi.tutrial.conventionplugin.buildlogic"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -26,15 +26,39 @@ java {
 }
 
 dependencies {
-//    compileOnly(libs.android.gradlePlugin)
-//    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
 }
 
 gradlePlugin {
     plugins {
         register("androidApplication") {
-            id = "nowinandroid.android.application"
+            id = "gradleconvention.android.application"
             implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidApplicationCompose") {
+            id = "gradleconvention.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "gradleconvention.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "gradleconvention.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidFeature") {
+            id = "gradleconvention.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+        register("androidTest") {
+            id = "gradleconvention.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "gradleconvention.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
         }
     }
 }
